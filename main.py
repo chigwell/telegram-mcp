92a0dc31b58912bc39632d27b88bccde5c915df0
Yurii Karabas <1998uriyyo@gmail.com>
2025-12-18 23:47:12 +0100

    fix lint errors

--- main.py
+++ main.py
@@ -3155,4 +3155,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- session_string_generator.py
+++ session_string_generator.py
@@ -104,5 +104,5 @@ def main() -> None:
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
